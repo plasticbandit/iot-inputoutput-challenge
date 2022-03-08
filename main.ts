e9dc61249af1f9c80973223030a857f8f6ebd60d
Nicola McFadden <72537387+plasticbandit@users.noreply.github.com>
2022-03-08 16:02:25 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+enum RadioMessage {
+    message1 = 49434,
+    Hello = 49337
+}
+input.onSound(DetectedSound.Loud, function () {
+	
+})
+radio.sendMessage(RadioMessage.Hello)
+music.playMelody("C5 A B G A F G E ", 500)
+soundExpression.hello.playUntilDone()
+basic.showIcon(IconNames.Happy)
+basic.forever(function () {
+	
+})
