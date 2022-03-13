bb41fb94903a14c34847cdbb493f417a82d84a36
Nicola McFadden <72537387+plasticbandit@users.noreply.github.com>
2022-03-13 11:11:15 -0400

    Added intro sequence

--- main.ts
+++ main.ts
@@ -6,9 +6,9 @@ input.onSound(DetectedSound.Loud, function () {
 	
 })
 radio.sendMessage(RadioMessage.Hello)
-music.playMelody("C5 A B G A F G E ", 500)
+music.playMelody("E B C5 A B G A F ", 500)
 soundExpression.hello.playUntilDone()
-basic.showIcon(IconNames.Happy)
+basic.showIcon(IconNames.Angry)
 basic.forever(function () {
 	
 })
