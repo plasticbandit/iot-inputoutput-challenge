afc22ad0c8942b3f05c0d3f502a02a5583038cd6
Nicola McFadden <72537387+plasticbandit@users.noreply.github.com>
2022-03-13 13:27:40 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,13 +2,19 @@ enum RadioMessage {
     message1 = 49434,
     Hello = 49337
 }
+function isBabyCrying () {
+	
+}
 input.onSound(DetectedSound.Loud, function () {
-	
+    babyCrying = 1
 })
-radio.sendMessage(RadioMessage.Hello)
-music.playMelody("E B C5 A B G A F ", 500)
+let babyCrying = 0
+input.setSoundThreshold(SoundThreshold.Loud, 125)
+babyCrying = 0
+radio.sendString("Start")
 soundExpression.hello.playUntilDone()
-basic.showIcon(IconNames.Angry)
-basic.forever(function () {
-	
-})
+basic.showIcon(IconNames.Happy)
+if (babyCrying) {
+    music.playMelody("E B C5 A B G A F ", 500)
+    basic.showIcon(IconNames.Angry)
+}
