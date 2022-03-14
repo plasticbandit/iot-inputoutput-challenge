87fcfd57cdae2aed9997c2a5d39bf7179203aecc
Dan Lundy <72224080+lundydaniel@users.noreply.github.com>
2022-03-14 16:29:23 -0400

    song of time zelda micro:bit edition

--- main.ts
+++ main.ts
@@ -1,55 +1,33 @@
-enum RadioMessage {
-    Hello = 49337,
-    message1 = 49434,
-    Crying = 58844
-}
-radio.onReceivedNumber(function (receivedNumber) {
-    isBabyCrying = receivedNumber
-})
 input.onButtonPressed(Button.A, function () {
-    isChildDevice = 0
-    radio.sendString("NewParent")
-    basic.showIcon(IconNames.Yes)
+    music.playTone(440, music.beat(BeatFraction.Breve))
+    music.rest(music.beat(BeatFraction.Whole))
+    music.playTone(294, music.beat(BeatFraction.Breve))
+    music.rest(music.beat(BeatFraction.Whole))
+    music.playTone(349, music.beat(BeatFraction.Breve))
+    music.rest(music.beat(BeatFraction.Whole))
+    music.playTone(440, music.beat(BeatFraction.Breve))
+    music.rest(music.beat(BeatFraction.Whole))
+    music.playTone(294, music.beat(BeatFraction.Breve))
+    music.rest(music.beat(BeatFraction.Whole))
+    music.playTone(349, music.beat(BeatFraction.Breve))
+    music.rest(music.beat(BeatFraction.Whole))
+    music.playTone(440, music.beat(BeatFraction.Double))
+    music.playTone(523, music.beat(BeatFraction.Double))
+    music.playTone(494, music.beat(BeatFraction.Double))
+    music.rest(music.beat(BeatFraction.Whole))
+    music.playTone(392, music.beat(BeatFraction.Double))
+    music.rest(music.beat(BeatFraction.Whole))
+    music.playTone(349, music.beat(BeatFraction.Double))
+    music.playTone(392, music.beat(BeatFraction.Double))
+    music.playTone(440, music.beat(BeatFraction.Double))
+    music.rest(music.beat(BeatFraction.Whole))
+    music.playTone(294, music.beat(BeatFraction.Double))
+    music.rest(music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Breve))
+    music.playTone(330, music.beat(BeatFraction.Breve))
+    music.playTone(294, music.beat(BeatFraction.Breve))
 })
-input.onSound(DetectedSound.Loud, function () {
-    if (isChildDevice == 1) {
-        radio.sendNumber(1)
-    }
-})
-radio.onReceivedString(function (receivedString) {
-    if (receivedString == "NewParent") {
-        isChildDevice = 1
-        basic.showIcon(IconNames.Heart)
-    }
-})
-input.onButtonPressed(Button.B, function () {
-    isBabyCrying = 0
-})
-input.onSound(DetectedSound.Quiet, function () {
-    if (isChildDevice == 1) {
-        radio.sendNumber(0)
-    }
-})
-let isBabyCrying = 0
-let isChildDevice = 0
-radio.setGroup(0)
-isChildDevice = 1
-let soundThreshold = 120
-input.setSoundThreshold(SoundThreshold.Loud, soundThreshold)
-if (input.soundLevel() >= soundThreshold) {
-    radio.sendNumber(1)
-} else {
-    radio.sendNumber(0)
-}
+basic.showString("Press A To Play A Song")
 basic.forever(function () {
-    if (isChildDevice == 0) {
-        if (isBabyCrying == 1) {
-            music.playMelody("C5 A B G A F G E ", 248)
-            basic.showIcon(IconNames.Sad)
-        }
-        if (isBabyCrying == 0) {
-            basic.showIcon(IconNames.Yes)
-            music.stopMelody(MelodyStopOptions.All)
-        }
-    }
+	
 })
